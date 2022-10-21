e844aee80353c96eb8dada9d3dc2ae9d233e5c09
STBoyden <sam@stboyden.com>
2022-10-21 20:46:42 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/ohmyzsh/ohmyzsh/ oh-my-zsh
+if [[ ! -d ./oh-my-zsh ]]; then
+  git clone https://github.com/ohmyzsh/ohmyzsh/ oh-my-zsh
+fi
 
-ln -s $(pwd)/oh-my-zsh $HOME/.oh-my-zsh
+if [[ ! -d $HOME/.oh-my-zsh ]]; then
+  ln -s $(pwd)/oh-my-zsh $HOME/.oh-my-zsh
+fi
+
+if [[ -f $HOME/.zshrc ]]; then
+  mv $HOME/.zshrc $HOME/.zshrc.backup
+fi
+
+if [[ -f $HOME/.bashrc ]]; then
+  mv $HOME/.bashrc $HOME/.bashrc.backup
+fi
+
 ln -s $(pwd)/zshrc $HOME/.zshrc
+ln -s $(pwd)/bashrc $HOME/.bashrc
